a206e17f6a5818b54c6fa6edfa97dac38b828b20
Leonard-P <72754537+Leonard-P@users.noreply.github.com>
2021-03-07 15:15:57 +0100

    added create vectors

---
+++ create_vectors.js
@@ -0,0 +1,60 @@
+tf.loadLayersModel("base/model.json").then(createVectors);
+
+function preprocessInput(img)
+{
+    let imageSize = 350; 
+
+    img = tf.browser.fromPixels(img);
+    img = tf.image.resizeBilinear(img, [imageSize, imageSize]).toFloat();
+    img = img.div(tf.scalar(255.0));
+    mean = [0.485, 0.456, 0.406];
+    std = [0.229, 0.224, 0.225];
+
+    let indices = [
+        tf.tensor1d([0], "int32"),
+        tf.tensor1d([1], "int32"),
+        tf.tensor1d([2], "int32")
+    ];
+
+    let processedValues = {
+        red: tf.gather(img, indices[0], 2).sub(tf.scalar(mean[0])).div(tf.scalar(std[0])).reshape([imageSize*imageSize]),
+        green: tf.gather(img, indices[1], 2).sub(tf.scalar(mean[1])).div(tf.scalar(std[1])).reshape([imageSize*imageSize]),
+        blue: tf.gather(img, indices[2], 2).sub(tf.scalar(mean[2])).div(tf.scalar(std[2])).reshape([imageSize*imageSize]),
+    };
+
+    let processedTensor = tf.stack([
+        processedValues.red, processedValues.green, processedValues.blue
+    ], 1, "float32").reshape([imageSize, imageSize, 3]).expandDims();
+    return processedTensor;
+}
+
+loadImage = async img => {
+    return new Promise((resolve, reject) => {
+        img.onload = async () => {
+            resolve(true);
+        };
+    });
+};
+
+async function createVectors(model){
+    var vectors = [];
+    for(let i=0; i<=3; i++){
+        let img = new Image;
+        img.src = '/data/' + i.toString() + '.jpg';
+        await loadImage(img);
+        let imgPreprocessed = preprocessInput(img);
+        let vector = model.predict(imgPreprocessed).dataSync();
+        vectors.push(vector);
+    }
+    let vectorsJSON = JSON.stringify(vectors);
+    download(vectorsJSON, 'vectors.txt', 'text/plain');
+
+}
+
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
