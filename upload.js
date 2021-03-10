38b9e64438e5b134decca0b9e2a8b2f372183c49
Leonard-P <72754537+Leonard-P@users.noreply.github.com>
2021-03-10 17:14:23 +0100

    upload

--- upload.js
+++ upload.js
@@ -1,27 +1,62 @@
-function readURL(input) {
+function crop(url, aspectRatio) {
+    // we return a Promise that gets resolved with our canvas element
+    return new Promise((resolve) => {
+      // this image will hold our source image data
+      const inputImage = new Image();
+  
+      // we want to wait for our image to load
+      inputImage.onload = () => {
+        // let's store the width and height of our image
+        const inputWidth = inputImage.naturalWidth;
+        const inputHeight = inputImage.naturalHeight;
+  
+        // get the aspect ratio of the input image
+        const inputImageAspectRatio = inputWidth / inputHeight;
+  
+        // if it's bigger than our target aspect ratio
+        let outputWidth = inputWidth;
+        let outputHeight = inputHeight;
+        if (inputsImageAspectRatio > aspectRatio) {
+          outputWidth = inputHeight * aspectRatio;
+        } else if (inputImageAspectRatio < aspectRatio) {
+          outputHeight = inputWidth / aspectRatio;
+        }
+  
+        // calculate the position to draw the image at
+        const outputX = (outputWidth - inputWidth) * 0.5;
+        const outputY = (outputHeight - inputHeight) * 0.5;
+  
+        // create a canvas that will present the output image
+        const outputImage = document.createElement("canvas");
+  
+        // set it to the same size as the image
+        outputImage.width = outputWidth;
+        outputImage.height = outputHeight;
+  
+        // draw our image at position 0, 0 on the canvas
+        const ctx = outputImage.getContext("2d");
+        ctx.drawImage(inputImage, outputX, outputY);
+        resolve(outputImage);
+      };
+      inputImage.src = url;
+    });
+}
+
+async function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#imageResult')
-                .attr('src', e.target.result);
+            //console.log(e.target.result);
+            crop("url("+e.target.result+")", 1).then((img)=>{
+                //console.log(img);
+                $('#upload-area')
+                    .attr("style", "background-image: url(" +e.target.result + ")");
+                $('#upload-content')
+                    .attr("style", "display: none");
+            });
         };
+
         reader.readAsDataURL(input.files[0]);
     }
-}
-
-$(function () {
-    $('#upload').on('change', function () {
-        readURL(input);
-    });
-});
-
-var input = document.getElementById( 'upload' );
-var infoArea = document.getElementById( 'upload-label' );
-
-input.addEventListener( 'change', showFileName );
-function showFileName( event ) {
-  var input = event.srcElement;
-  var fileName = input.files[0].name;
-  infoArea.textContent = 'File name: ' + fileName;
 }
